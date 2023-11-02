7a542cc51c7a5f86c48da66ecfcfb9a4ee6b1a09
Shongwe Siyanda <shongwesiyanda@gmail.com>
2023-11-02 19:39:46 +0200

    configured vue 3 and also setup bootstrap as a css framework

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="fw-bold text-uppercase text-primary">test component</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Holidays App</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    @vite(['resources/sass/app.scss'])
+</head>
+<body>
+<div id="app">
+    <main class="container mx-auto py-lg-5 py-4">
+        <home-component></home-component>
+    </main>
+</div>
+@vite('resources/js/app.js')
+</body>
+</html>
+
+
+
+
+
