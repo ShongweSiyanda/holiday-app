4ad90cf16f41adc8276d9ac796b3a5ac9b274f23
Shongwe Siyanda <shongwesiyanda@gmail.com>
2023-11-04 17:10:36 +0200

    Application Primary Color Setup and Configs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 import {createApp} from 'vue/dist/vue.esm-bundler.js';
 import Home from './components/Home.vue';
+import multiselect from 'vue-multiselect';
+
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+
 
 const app = createApp({});
 
 app.component('home-component', Home);
+app.component('multi-select',multiselect);
 
 app.mount("#app");
