a1944c24f504f8dc15e0aa48ff8983be502ebb54
Shongwe Siyanda <shongwesiyanda@gmail.com>
2023-11-05 13:19:09 +0200

    Inject repository class into HolidayController and use the methods implemented in the class.

--- app/Http/Controllers/HolidayController.php
+++ app/Http/Controllers/HolidayController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Repositories\HolidayRepository;
 
 class HolidayController extends Controller
 {
 
+    private HolidayRepository $holidayRepository;
 
+    public function __construct(HolidayRepository $holidayRepository)
+    {
+        $this->holidayRepository = $holidayRepository;
+    }
+
+    public function getCountries(): JsonResponse
+    {
+        $countries = $this->holidayRepository->getCountries();
+        return response()->json($countries);
+    }
+
+    public function getHolidaysForYear(Request $request): JsonResponse
+    {
+        $country = $request->input('country');
+        $year = $request->input('year');
+
+        $holidays = $this->holidayRepository->getHolidaysForYear($country, $year);
+        return response()->json($holidays);
+    }
 }
